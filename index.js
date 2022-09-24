48adca00654a58d83f5269d88968eaa0db0ce594
Rahul Joshi <tj18022826@gmail.com>
2022-09-24 08:32:24 +0530

    Completed github bot

--- index.js
+++ index.js
@@ -1,67 +1,67 @@
 const builder = require('botbuilder');
 const restify = require('restify');
-const githubClient = require('./github-client');
-
+var githubClient = require('./github-client.js');
 
 const connector = new builder.ChatConnector();
-
 const bot = new builder.UniversalBot(connector)
-    .set('storage',new builder.MemoryBotStorage());
+    .set('storage', new builder.MemoryBotStorage());
 
-const dialog=new builder.IntentDialog();
+const dialog = new builder.IntentDialog();
 
-dialog.matches(/^search/i,[
-    function(session,args,next){
-        if(session.message.text.toLowerCase()==='search'){
-            builder.Prompts.text(session,'Who are youlooking for?');
-        } else{
-            var query=session.message.text.substring(7);
-            next({response:query});
+dialog.matches(/^search/i, [
+    function (session, args, next) {
+        if (session.message.text.toLowerCase() == 'search') {
+            builder.Prompts.text(session, 'Who are you looking for?');
+        } else {
+            var query = session.message.text.substring(7);
+            next({ response: query });
         }
     },
-    function(session,result,next){
-        var query=result.response;
-        if(!query){
+    function (session, result, next) {
+        var query = result.response;
+        if (!query) {
             session.endDialog('Request cancelled');
-        } else{
-            githubClient.executeSearch(query,function(profiles){
-                var totalCount=profiles.total_count;
-                if(totalCount===0){
+        } else {
+            githubClient.executeSearch(query, function (profiles) {
+                var totalCount = profiles.total_count;
+                if (totalCount == 0) {
                     session.endDialog('Sorry, no results found.');
-                } else if(totalCount>10){
-                    session.endDialog('Provide more restrictive querry');
-                } else{
-                    session.dialogData.property=null;
-                    var usernames=profiles.items.map(function(item){item.login});
-                    builder.Prompts.choice(session,'Select User');
+                } else if (totalCount > 10) {
+                    session.endDialog('More than 10 results were found. Please provide a more restrictive query.');
+                } else {
+                    session.dialogData.property = null;
+                    var usernames = profiles.items.map(function (item) { return item.login });
+                    builder.Prompts.choice(session, 'What user do you want to load?', usernames);
                 }
-            })
+            });
         }
-    },
-    function(session,result,next){
-        var username=result.response.entity;
-        githubClient.loadProfile(username,function(profile){
-            var card=new builder.ThumbnailCard();
-            card.title(profile.login);
-            card.images([builder.CardImage.create(session,profile.avatar_url)]);
-            if(profile.name) card.subtitle(profile.name);
+    }, function (session, result, next) {
+        var username = result.response.entity;
+        githubClient.loadProfile(username, function (profile) {
+            var card = new builder.ThumbnailCard(session);
 
-            var text='';
-            if(profile.company) text+=profile.company+'\n';
-            if(profile.email) text+=profile.email+'\n';
-            if(profile.bio) text+=profile.bio+'\n';
+            card.title(profile.login);
+
+            card.images([builder.CardImage.create(session, profile.avatar_url)]);
+
+            if (profile.name) card.subtitle(profile.name);
+
+            var text = '';
+            if (profile.company) text += profile.company + ' \n';
+            if (profile.email) text += profile.email + ' \n';
+            if (profile.bio) text += profile.bio;
             card.text(text);
 
-            card.tap(new builder.CardAction.openUrl(session,profile.html_url));
+            card.tap(new builder.CardAction.openUrl(session, profile.html_url));
 
-            var message=new builder.Message(session).attachments([card]);
+            var message = new builder.Message(session).attachments([card]);
             session.send(message);
-        })
+        });
     }
 ]);
 
-bot.dialog('/',dialog);
+bot.dialog('/', dialog);
 
 const server = restify.createServer();
-server.post('/api/message', connector.listen());
+server.post('/api/messages', connector.listen());
 server.listen(3978);
